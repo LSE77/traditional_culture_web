34c009790ecef44dff5c2d1a9585fc607fe213aa
LSE77 <129946184+LSE77@users.noreply.github.com>
2026-06-23 15:34:47 +0900

    레전드 삭제
    삭제엔딩

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { initRouter } from "./router.js";
-import { initHistoryPage } from "./history.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  initRouter();
-  initHistoryPage();
-
-  console.log("main.js 실행됨");
-});
--- js/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-// Supabase 프로젝트 URL
-const SUPABASE_URL = "https://xzjhwifmtixfrtoanwip.supabase.co";
-
-// Supabase publishable key를 여기에 붙여넣기
-const SUPABASE_KEY = "sb_publishable_YUqjNXZr2wPpb3EvdmEidw_4kbMo4la";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
